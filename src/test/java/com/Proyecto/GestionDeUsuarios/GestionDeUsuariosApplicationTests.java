a30adbc9208cce4a15ee07e7167ec1b2d8af3893
AlfredoRosales12 <rosales.alfredo.goo@gmail.com>
2025-10-31 17:46:31 -0600

    Se inicializa el proyecto

---
+++ src/test/java/com/Proyecto/GestionDeUsuarios/GestionDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto.GestionDeUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
